f618172a42a87fd2b3c393565cbda50ab1168c96
Tatz884 <tatsuya884.0201@gmail.com>
2023-06-25 16:59:09 +0900

    tasks display in sorted order, and duplicates and empty tasks are removed

--- To do list/scripts/main.js
+++ To do list/scripts/main.js
@@ -12,30 +12,28 @@ addBtn.addEventListener("click", () => {
   
 })
 
+
 clearAllBtn.addEventListener("click", () => {
     const currStorageLength = localStorage.length
     // clean all displayed tasks:
     while (unordList.firstChild) {
         unordList.removeChild(unordList.firstChild);
-      }
-    // remove all tasks in localStorage
-    for (var i = 0; i < currStorageLength; i++) {
-        localStorage.removeItem(localStorage.key(0));
     }
-
+    // remove all tasks in localStorage
+    localStorage.clear();
 })
 
 function storeData(currText) {
     // store a task written on the form
 
-    // const currStorageLength = localStorage.length
     console.log(`storing data... localStorage.length = ${localStorage.length}`)
 
-    localStorage[`${currText}`] = currText
+    // store with timestamp
+    var d=new Date();
+    var timestamp = d.getTime();
+    localStorage.setItem(`${timestamp}`, `${currText}`);
     // Note about this code:
-    // Due to unknown reason, this line 34 seems to be very buggy.
-    // 
-    // Other localStorage[`${localStorage.length}`] = currText or
+    // Somehow localStorage[`${localStorage.length}`] = currText or
     // localStorage.setItem(localStorage.length) = currText will be executed multiple times and
     // thus are not appropriate here.
     // Since the console.log above is executed only once, storeData triggering seems to be fine.
@@ -48,20 +46,32 @@ function storeData(currText) {
 function displayTasks() {
     // display all tasks currently stored in the web Storage API
     // clean all displayed tasks:
+
+    removeDuplicates();
+    removeEmptyTask();
+
+    // return the sorted array of associative array, like:
+    // [{111111111: "a"}, {111111113: "b"}]
+    const sortedNestedArray = Object.keys(localStorage)
+        .map(key => key)
+        .sort() // alphabetic sort
+        .map(key => ({[key]: localStorage[key] }))
+
+
     while (unordList.firstChild) {
         unordList.removeChild(unordList.firstChild);
       }
 
-    // read localStorage.length
-    // then populate for it one by one
+    // Populate tasks one by one
     if (localStorage.length === 0) {
         // pass
     } else {
-        for (var i = 0; i < localStorage.length; i++) {
-            const id = localStorage.key(i)
-            const task = localStorage.getItem(`${id}`)
-            
-            console.log(id)
+        for (timeAndTask of sortedNestedArray) {
+
+            const timestamp = Object.keys(timeAndTask)[0]
+            const task = localStorage.getItem(timestamp)
+
+            console.log(timestamp)
 
             const listItem = document.createElement("li");
             const span = document.createElement("span");
@@ -73,17 +83,41 @@ function displayTasks() {
             span.textContent = task;
             doneBtn.textContent = 'Done';
             unordList.appendChild(listItem);
-            doneBtn.addEventListener("click", (e) => {
-                localStorage.removeItem(id)
-                e.parentNode.remove();
+            doneBtn.addEventListener("click", () => {
+                localStorage.removeItem(timestamp)
                 displayTasks();
             })
 
         }
     }
     console.log(localStorage);
+}
 
-    // pass
+
+// Without this function, somehow the localStorage.setItem in storeData()
+// is executed multiple times, resulting in multiple entries
+function removeDuplicates() {
+    var entrySoFar = [];
+    for (var i = 0; i < localStorage.length; i++) {
+        // console.log(`key is ${i}`)
+        // console.log(`entry so far is ${entrySoFar}`)
+        // console.log(`localStorage.key(i) is ${localStorage.key(i)}`)
+        if (entrySoFar.includes(localStorage.getItem(localStorage.key(i)))) {
+            localStorage.removeItem(localStorage.key(i))
+        } else {
+            entrySoFar.push(localStorage.getItem(localStorage.key(i)))
+        }
+    }
+}
+
+// Without this function, somehow the localStorage.setItem in storeData()
+// may be executed on empty form, resulting in empty entries
+function removeEmptyTask() {
+    for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.getItem(localStorage.key(i)) === '') {
+            localStorage.removeItem(localStorage.key(i))
+        } 
+    }
 }
 
 displayTasks()
