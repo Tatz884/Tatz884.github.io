dae58f7e62c2d3c7e409a1c9c96fcd8b96a701a7
Tatz <tatsuya884.0201@gmail.com>
2023-07-07 18:06:40 +0500

    Enable access from frontend to backend with CORS setting

--- To-do-list/backend/api/main.py
+++ To-do-list/backend/api/main.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 
 from backend.api.routers import task, done
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "https://example.org"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(task.router)
 app.include_router(done.router)
